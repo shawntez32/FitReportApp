5549ce9b468991de6aaab1bcc38083125d5f7ec4
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-10-23 16:56:29 -0400

    Update home.dart

--- screens/home.dart
+++ screens/home.dart
@@ -15,97 +15,83 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Fit Report"),
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/thirsty.jpg"))),
-        child: ListView(children: [
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              hoverColor: Color.fromARGB(143, 114, 173, 180),
-              title: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SetGoalPage()),
-                  );
-                },
-                child: Text(
-                  'New Goals',
-                  style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          title: Text("Fit Report"),
+        ),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/thirsty.jpg"))),
+            child: Expanded(
+              child: Row(children: [
+                Expanded(
+                  child: Column(children: [
+                    Container(
+                      height: 60,
+                      child: Card(
+                        child: ListTile(
+                          tileColor: Colors.blue,
+                          hoverColor: Color.fromARGB(143, 114, 173, 180),
+                          title: TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SetGoalPage()),
+                              );
+                            },
+                            child: Text(
+                              'New Goals',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
                 ),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              title: TextButton(
-                onPressed: null,
-                child:
-                    Text('Track Goals', style: TextStyle(color: Colors.black)),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              title: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CalLogPage()),
-                  );
-                },
-                child:
-                    Text('Calorie Log', style: TextStyle(color: Colors.black)),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              title: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FitLogPage()),
-                  );
-                },
-                child:
-                    Text('Fitness Log', style: TextStyle(color: Colors.black)),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              title: TextButton(
-                onPressed: null,
-                child: Text('Sheets', style: TextStyle(color: Colors.black)),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              tileColor: Colors.blue,
-              title: TextButton(
-                onPressed: null,
-                child: Text('Progress Report',
-                    style: TextStyle(color: Colors.black)),
-              ),
-            ),
-          ),
-          Container(
-            height: 300.0,
-          )
-        ]),
-      ),
-    );
+                Expanded(
+                  child: Column(children: [
+                    Container(
+                      height: 60,
+                      child: Card(
+                        child: ListTile(
+                          tileColor: Colors.blue,
+                          title: TextButton(
+                            onPressed: null,
+                            child: Text('Track Goals',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                Expanded(
+                  child: Column(children: [
+                    Container(
+                      height: 60,
+                      child: Card(
+                        child: ListTile(
+                          tileColor: Colors.blue,
+                          title: TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => CalLogPage()),
+                              );
+                            },
+                            child: Text('Calorie Log',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+              ]),
+            )));
   }
 }
