3c24182b5b5afba8bb83f008fd9b3206dd6ea5ba
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-10-29 13:27:46 -0400

    Update home.dart

--- screens/home.dart
+++ screens/home.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
+import 'package:app/screens/homeroutes/setFitGoals.dart';
 import "package:flutter/material.dart";
 import 'package:http/http.dart' as http;
 import 'homeroutes/dailyLogCal.dart';
@@ -18,143 +19,246 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text("Fit Report"),
         ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/deadlift.jpg"),
-                  fit: BoxFit.cover)),
-          child: ListView(children: [
-            SizedBox(
-              height: 40,
-            ),
-            Expanded(
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      height: 180,
-                      width: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(children: [
-                        const Image(
-                          height: 100,
-                          fit: BoxFit.cover,
-                          image: AssetImage('assets/images/yoga.jpg'),
-                        ),
-                        Card(
-                          child: TextButton(
-                              onPressed: null,
-                              child: Text(
-                                "Calorie Goals",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          color: Colors.blue,
-                        ),
-                        Text(
-                          "Set up Calorie Goals",
-                          textAlign: TextAlign.center,
-                        ),
-                      ]),
-                    ),
-                    Container(
-                      height: 180,
-                      width: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(children: [
-                        const Image(
-                          height: 100,
-                          fit: BoxFit.cover,
-                          image: AssetImage('assets/images/squat.jpg'),
-                        ),
-                        Card(
-                          child: TextButton(
-                              onPressed: null,
-                              child: Text(
-                                "Workout Goals",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          color: Colors.blue,
-                        ),
-                        Text(
-                          "Set up Workout Goals",
-                          textAlign: TextAlign.center,
-                        ),
-                      ]),
-                    ),
-                  ]),
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Expanded(
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      height: 180,
-                      width: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(children: [
-                        const Image(
-                          height: 100,
-                          fit: BoxFit.cover,
-                          image: AssetImage('assets/images/thirsty.jpg'),
-                        ),
-                        Card(
-                          child: TextButton(
-                              onPressed: null,
-                              child: Text(
-                                "Calorie Log",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          color: Colors.blue,
-                        ),
-                        Text(
-                          "Add to Calorie Log",
-                          textAlign: TextAlign.center,
-                        ),
-                      ]),
-                    ),
-                    Container(
-                      height: 180,
-                      width: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(children: [
-                        const Image(
-                          height: 100,
-                          width: 120,
-                          fit: BoxFit.fill,
-                          image: AssetImage('assets/images/weights.jpg'),
-                        ),
-                        Card(
-                          child: TextButton(
-                              onPressed: null,
-                              child: Text(
-                                "Fitness Log",
-                                style: TextStyle(color: Colors.white),
-                              )),
-                          color: Colors.blue,
-                        ),
-                        Text(
-                          "Add to Fitness Log",
-                          textAlign: TextAlign.center,
-                        ),
-                      ]),
-                    ),
-                  ]),
-            ),
-          ]),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/deadlift.jpg"),
+                    fit: BoxFit.cover)),
+            child: ListView(children: [
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/yoga.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SetGoalPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Calorie Goals",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Set up Calorie Goals",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/squat.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SetFitGoalPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Workout Goals",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Set up Workout Goals",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/thirsty.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SetGoalPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Calorie Log",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Add to Calorie Log",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            width: 120,
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/images/weights.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => FitLogPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Fitness Log",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Add to Fitness Log",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/thirsty.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SetGoalPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Fitness Report",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Analyze your accountability grade and habits.",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(children: [
+                          const Image(
+                            height: 100,
+                            width: 120,
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/images/weights.jpg'),
+                          ),
+                          Card(
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => FitLogPage()),
+                                  );
+                                },
+                                child: Text(
+                                  "Track Goals",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Look at how far you've come through your journey.",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      ),
+                    ]),
+              ),
+            ]),
+          ),
         ));
   }
 }
