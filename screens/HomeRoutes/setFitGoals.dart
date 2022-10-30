936f6dae12a1ba5722a3309d4e06a13499907199
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-10-30 05:25:04 -0400

    Update setFitGoals.dart

--- screens/HomeRoutes/setFitGoals.dart
+++ screens/HomeRoutes/setFitGoals.dart
@@ -15,44 +15,61 @@ class _SetFitGoalPageState extends State<SetFitGoalPage> {
       ),
       body: SingleChildScrollView(
         child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/deadlift.jpg"),
+                  fit: BoxFit.cover)),
           height: MediaQuery.of(context).size.height,
-          color: Color.fromARGB(255, 114, 237, 114),
           child: ListView(children: [
             Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 1')),
-            ),
-            Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 2')),
-            ),
-            Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 3')),
-            ),
-            Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 4')),
-            ),
-            Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 5')),
-            ),
-            Container(
-              height: 30,
-              child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Workout 6')),
+              height: 200,
+              color: Colors.white,
+              child: ListView(
+                children: [
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 1')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 2')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 3')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 4')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 5')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Workout 6')),
+                  ),
+                ],
+              ),
             ),
             Container(
               height: 25.0,
