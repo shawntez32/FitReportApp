5b79570d300d5cf0edd0e03106806b161f28e3d9
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-10-30 05:05:57 -0400

    Update setGoals.dart

--- screens/HomeRoutes/setGoals.dart
+++ screens/HomeRoutes/setGoals.dart
@@ -20,61 +20,65 @@ class _SetGoalPageState extends State<SetGoalPage> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  colors: <Color>[
-                Color.fromARGB(255, 218, 196, 89),
-                Color.fromARGB(255, 114, 237, 114)
-              ])),
+              image: DecorationImage(
+                  image: AssetImage("assets/images/deadlift.jpg"),
+                  fit: BoxFit.cover)),
           child: ListView(children: [
-            Container(
+            SizedBox(
               height: 30,
-              color: Colors.blue,
-              child: TextField(
-                decoration: InputDecoration(labelText: "Current Weight"),
+            ),
+            Container(
+              height: 40,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Current Weight"),
+                ),
               ),
             ),
             Container(
-              height: 30,
-              color: Colors.blue,
-              child: TextField(
-                decoration: InputDecoration(labelText: "Goal Weight"),
+              height: 40,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Goal Weight"),
+                ),
               ),
             ),
             Container(
-              height: 30,
-              color: Colors.blue,
-              child: TextField(
-                decoration: InputDecoration(labelText: "Start Date"),
+              height: 40,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Start Date"),
+                ),
               ),
             ),
             Container(
-              height: 30,
-              color: Colors.blue,
-              child: TextField(
-                decoration: InputDecoration(labelText: "End Date"),
+              height: 40,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(labelText: "End Date"),
+                ),
               ),
             ),
             Container(
-              height: 30,
-              color: Colors.blue,
-              child: TextField(
-                decoration: InputDecoration(labelText: "Caloric Intake Goal"),
+              height: 40,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Caloric Intake Goal"),
+                ),
               ),
             ),
             Container(
               height: 200.0,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      colors: <Color>[
-                    Color.fromARGB(255, 218, 196, 89),
-                    Color.fromARGB(255, 114, 237, 114)
-                  ])),
               child: ListView(
                 children: [
                   Padding(
