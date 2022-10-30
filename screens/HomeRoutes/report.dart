e55ae267b4d1b5752e3b232ccf08a83cfbc2c360
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-10-30 14:04:49 -0400

    Update report.dart

--- screens/HomeRoutes/report.dart
+++ screens/HomeRoutes/report.dart
@@ -1,53 +1,72 @@
 import "package:flutter/material.dart";
 import 'package:http/http.dart' as http;
 
-class SetGoalPage extends StatefulWidget {
+class ReportPage extends StatefulWidget {
   @override
-  State<SetGoalPage> createState() => _SetGoalPageState();
+  State<ReportPage> createState() => _ReportPageState();
 }
 
-class _SetGoalPageState extends State<SetGoalPage> {
+class _ReportPageState extends State<ReportPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Fitness Report"),
+      ),
       body: SingleChildScrollView(
         child: Container(
-          height: 800.0,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/deadlift.jpg"),
+                  fit: BoxFit.cover)),
+          height: MediaQuery.of(context).size.height,
           child: ListView(children: [
-            ListTile(
-              tileColor: Colors.blue,
-              title: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Weight Goal')),
-            ),
-            ListTile(
-              tileColor: Colors.blue,
-              title: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Current Weight')),
-            ),
-            ListTile(
-              tileColor: Colors.blue,
-              title: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Timeframe(Days)')),
-            ),
-            ListTile(
-              tileColor: Colors.blue,
-              title: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Caloric Intake Goal')),
+            SizedBox(
+              height: 30,
             ),
             Container(
-              height: 100.0,
+              height: 220,
+              color: Colors.white,
               child: ListView(
                 children: [
-                  TextButton(
-                    onPressed: null,
-                    child: Text('Fitness Goals'),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Weight Goal')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Current Weight')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Timeframe(Days)')),
+                  ),
+                  Container(
+                    height: 30,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Caloric Intake Goal')),
+                  ),
+                  Container(
+                    height: 100.0,
+                    child: ListView(
+                      children: [
+                        TextButton(
+                          onPressed: null,
+                          child: Text('Fitness Goals'),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
