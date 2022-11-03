e711848907f9bda52a5c547b892914a4435491d1
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-11-03 19:51:55 -0400

    Update login.dart

--- screens/login.dart
+++ screens/login.dart
@@ -11,11 +11,59 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+var loginSuccessful = false;
+var emailController = TextEditingController();
+var pwordController = TextEditingController();
+var userId;
+var fname;
+var lname;
+var userEmail;
+var userPword;
+var age;
+var userAddress;
+var i = 0;
+
 class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
-
+  final _pwordController = TextEditingController();
   var stringResponse;
   var mapResponse;
+  Future loginVer(email, pword) async {
+    var apiUrl = 'https://fitreportusers22.herokuapp.com/Login';
+    var client = http.Client();
+    var uri = Uri.parse(apiUrl);
+    var response = await client.get(uri);
+    print(response.body);
+    List list = json.decode(response.body);
+    i = 0;
+    while (i < list.length) {
+      var x = list[i]["_id"];
+      var e = list[i]["email"];
+      var p = list[i]["pword"];
+      if ((email + pword).toString() == (e + p).toString()) {
+        setState(() {
+          loginSuccessful = true;
+          userId = list[i]["_id"];
+          userEmail = list[i]["email"];
+          userPword = list[i]["pword"];
+          fname = list[i]["fname"];
+          lname = list[i]["lname"];
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        });
+      } else {
+        loginSuccessful = false;
+        print("unsuccessful");
+      }
+      print(i);
+      print(list[i]["email"] + list[i]["pword"]);
+      print(email + pword);
+      i = i + 1;
+    }
+    return email;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,41 +75,49 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Container(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: <Color>[
-                  Color.fromARGB(255, 218, 196, 89),
-                  Color.fromARGB(255, 114, 237, 114)
-                ])),
-            height: 800.0,
+                image: DecorationImage(
+                    image: AssetImage("assets/images/deadlift.jpg"),
+                    fit: BoxFit.cover)),
+            height: MediaQuery.of(context).size.height,
             child: ListView(children: [
               SizedBox(height: 35),
-              Text(
-                'Fitness Report',
-                style: TextStyle(fontSize: 22.0),
-                textAlign: TextAlign.center,
+              Padding(
+                padding: EdgeInsets.fromLTRB(75, 25, 75, 25),
+                child: Container(
+                  color: Colors.blue,
+                  height: 42,
+                  child: Text(
+                    'Fitness Report',
+                    style: TextStyle(fontSize: 22.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
               SizedBox(
                 height: 25,
               ),
               Padding(
-                padding: EdgeInsets.all(24.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(24.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
+                padding: EdgeInsets.all(44.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.white,
+                  child: ListView(children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    TextField(
+                      controller: _pwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ]),
                 ),
               ),
               Padding(
@@ -70,10 +126,7 @@ class _LoginPageState extends State<LoginPage> {
                   height: 50.0,
                   child: ElevatedButton(
                     onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomePage()),
-                      );
+                      loginVer(_emailController.text, _pwordController.text);
                     },
                     style: TextButton.styleFrom(
                       backgroundColor: Colors.blue,
