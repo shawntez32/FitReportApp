3165dcbf4df3575508446094c773d6545de28923
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-11-06 10:48:35 -0500

    Add files via upload

---
+++ screens/HomeRoutes/pressed.dart
@@ -0,0 +1,150 @@
+bool sunClicked1 = false;
+bool satClicked1 = false;
+bool monClicked1 = false;
+bool tuesClicked1 = false;
+bool wedClicked1 = false;
+bool thurClicked1 = false;
+bool friClicked1 = false;
+
+bool sunClicked2 = false;
+bool satClicked2 = false;
+bool monClicked2 = false;
+bool tuesClicked2 = false;
+bool wedClicked2 = false;
+bool thurClicked2 = false;
+bool friClicked2 = false;
+
+bool sunClicked3 = false;
+bool satClicked3 = false;
+bool monClicked3 = false;
+bool tuesClicked3 = false;
+bool wedClicked3 = false;
+bool thurClicked3 = false;
+bool friClicked3 = false;
+
+bool sunClicked4 = false;
+bool satClicked4 = false;
+bool monClicked4 = false;
+bool tuesClicked4 = false;
+bool wedClicked4 = false;
+bool thurClicked4 = false;
+bool friClicked4 = false;
+
+bool sunClicked5 = false;
+bool satClicked5 = false;
+bool monClicked5 = false;
+bool tuesClicked5 = false;
+bool wedClicked5 = false;
+bool thurClicked5 = false;
+bool friClicked5 = false;
+
+bool sunClicked6 = false;
+bool satClicked6 = false;
+bool monClicked6 = false;
+bool tuesClicked6 = false;
+bool wedClicked6 = false;
+bool thurClicked6 = false;
+bool friClicked6 = false;
+
+int sunPressClicked1 = 3;
+int satPressClicked1 = 3;
+int monPressClicked1 = 3;
+int tuesPressClicked1 = 3;
+int wedPressClicked1 = 3;
+int thurPressClicked1 = 3;
+int friPressClicked1 = 3;
+
+int sunPressClicked2 = 3;
+int satPressClicked2 = 3;
+int monPressClicked2 = 3;
+int tuesPressClicked2 = 3;
+int wedPressClicked2 = 3;
+int thurPressClicked2 = 3;
+int friPressClicked2 = 3;
+
+int sunPressClicked3 = 3;
+int satPressClicked3 = 3;
+int monPressClicked3 = 3;
+int tuesPressClicked3 = 3;
+int wedPressClicked3 = 3;
+int thurPressClicked3 = 3;
+int friPressClicked3 = 3;
+
+int sunPressClicked4 = 3;
+int satPressClicked4 = 3;
+int monPressClicked4 = 3;
+int tuesPressClicked4 = 3;
+int wedPressClicked4 = 3;
+int thurPressClicked4 = 3;
+int friPressClicked4 = 3;
+
+int sunPressClicked5 = 3;
+int satPressClicked5 = 3;
+int monPressClicked5 = 3;
+int tuesPressClicked5 = 3;
+int wedPressClicked5 = 3;
+int thurPressClicked5 = 3;
+int friPressClicked5 = 3;
+
+int sunPressClicked6 = 3;
+int satPressClicked6 = 3;
+int monPressClicked6 = 3;
+int tuesPressClicked6 = 3;
+int wedPressClicked6 = 3;
+int thurPressClicked6 = 3;
+int friPressClicked6 = 3;
+
+List wk1Pressed = [
+  sunClicked1,
+  monClicked1,
+  tuesClicked1,
+  wedClicked1,
+  thurClicked1,
+  friClicked1,
+  satClicked1
+];
+List wk2Pressed = [
+  sunClicked2,
+  monClicked2,
+  tuesClicked2,
+  wedClicked2,
+  thurClicked2,
+  friClicked2,
+  satClicked2
+];
+List wk3Pressed = [
+  sunClicked3,
+  monClicked3,
+  tuesClicked3,
+  wedClicked3,
+  thurClicked3,
+  friClicked3,
+  satClicked3
+];
+List wk4Pressed = [
+  sunClicked4,
+  monClicked4,
+  tuesClicked4,
+  wedClicked4,
+  thurClicked4,
+  friClicked4,
+  satClicked4
+];
+List wk5Pressed = [
+  sunClicked5,
+  monClicked5,
+  tuesClicked5,
+  wedClicked5,
+  thurClicked5,
+  friClicked5,
+  satClicked5
+];
+List wk6Pressed = [
+  sunClicked6,
+  monClicked6,
+  tuesClicked6,
+  wedClicked6,
+  thurClicked6,
+  friClicked6,
+  satClicked6
+];
